a9fda0642455da7b680979783976453fcbda2e67
brookeshary <144834940+brookeshary@users.noreply.github.com>
2023-10-31 09:55:31 -0400

    Final_01

--- Monarch_movement.js
+++ Monarch_movement.js
@@ -1,20 +1,24 @@
 //CONSTANTS
-const NUM_CIRCLES = 50;
-const FLOAT_SPEED =1.0;
-const TRANSITION_SPEED = 0.05;
-const CIRCLE_SIZE_DIVISOR = 25;
+const NUM_CIRCLES = 200;
+const FLOAT_SPEED =2;
+const TRANSITION_SPEED = .04;
+const CIRCLE_SIZE_DIVISOR = 80;
 
 //VARIABLES
 let circles = [];
 let float = true;
 let circleSize;
+let font;
+let fontSize;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
 
+
   //create initialize circle objects with position and speed
-  for (let i = 0; i <NUM_CIRCLES; i++) {
+  for (let i = 0; i < NUM_CIRCLES; i++) {
     circleSize = width / CIRCLE_SIZE_DIVISOR;
     let circle = {
       x: random(circleSize/2, width - circleSize/2),
@@ -31,29 +35,36 @@ function setup() {
 function draw() {
   clear(); // Clear the background (translucent) each frame
 
-  //update and draw each circle sin the array
+
+  //update and draw each circle in the array
   for (let i = 0; i < circles.length; i++) {
     let circle = circles[i];
-    if (float) {
+      if (float) {
+        // Set the fill color to red (RGB)
+        fill(244, 238, 229);
       //update the circles position based on speed
-    circle.x += circle.speedX;
-    circle.y += circle.speedY;
+    circle.x += circle.speedX + random(-2, 2);
+    circle.y += circle.speedY + random(-2, 2);
 
       //check for collisions with edges and reverse speed if necessary
     if (circle.x < 0 + circleSize /2 || circle.x > width - circleSize /2) {
       circle.speedX = -circle.speedX;
     }
 
-    if (circle.y < 0 + circleSize /2 || circle.y > width - circleSize /2) {
+    if (circle.y < 0 + circleSize /2 || circle.y > height - circleSize /2) {
       circle.speedY = -circle.speedY;
     }
     } else {
       //calculate the width needed for the circles
-    let spacing = width / NUM_CIRCLES;
+    let spacing = width / NUM_CIRCLES*2;
 
       //set target positions for the lined up state
-      circle.targetX = i * spacing + circleSize * 0.75;
-      circle.targetY = height / 2;
+      //circle.targetX = i * spacing + circleSize * 0.75;
+      //circle.targetY = height;
+
+      //set target positions for the lined up state
+      circle.targetX = mouseX;
+      circle.targetY = mouseY;
 
       //animate the transition to the target position using Lerp
       circle.x = lerp(circle.x, circle.targetX, TRANSITION_SPEED);
@@ -64,9 +75,6 @@ function draw() {
     ellipse (circle.x, circle.y, circleSize, circleSize)
   }
 }
-//  function mouseWheel () {
-  //  float = false;
-//  }
 
   function mouseClicked() {
     float = !float;
